da0ae4db02ed4bb23c1aa22162785ad80262d2c1
student1432 <84581003+student1432@users.noreply.github.com>
2022-04-12 15:40:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload()
+{
+
+}
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(150, 290);
+}
+function draw()
+{
+    circle(30, 30, 20);
+    rect(40, 25, 550, 20);
+    circle(600, 30, 20);
+    rect(40, 25, 25, 450);
+    rect(40, 450, 550, 20);
+    circle(30, 450, 20);
+    rect(570, 25, 25, 450);
+    circle(600, 450, 20)
+}
